9e1dcd2112901daeaecde3f6fbf1121999e1b2bf
Charlie <Charlie@CBeirnaert-20150618-092033.uantwerpen.be>
2017-07-19 15:15:21 +0200

    added unStructureBatches function

---
+++ R/unStructureBatches.R
@@ -0,0 +1,31 @@
+#' obtain all datasets in a single list for easy processing with purrr
+#'
+#'
+#' @param xcmsSets.Post.batchSplit list of xcmsSet objects after Batch splitting (BatchSplitR function)
+#' @param BatchProcessingStructure The batch processing structure (obtained with getBatchStructure)
+#' 
+#' @return unStructuredBatchList a list with every individual batch in a list element
+#' 
+#'
+#' @author Charlie Beirnaert, \email{charlie.beirnaert@@uantwerpen.be}
+#'
+#' @examples
+#' 
+#' 
+#' @export
+unStructureBatches = function(xcmsSets.Post.batchSplit, BatchProcessingStructure){
+    
+    nMulitBatches = BatchProcessingStructure$MultiBatchData
+    nBatches = sum(BatchProcessingStructure$nBatches)
+    unStructuredBatchList = vector("list", length = nBatches )
+    
+    cter = 1
+    for(k in 1:nMulitBatches){
+        for(j in 1:BatchProcessingStructure$nBatches[k]){
+            unStructuredBatchList[[cter]] = xcmsSets.Post.batchSplit[[k]][[j]]
+            cter = cter + 1
+        }
+    }
+    
+    return(unStructuredBatchList)
+}
