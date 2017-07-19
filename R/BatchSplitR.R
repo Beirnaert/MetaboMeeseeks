393e24ae2d01b571bbe7a5fcfb6e751402a69c62
Charlie <Charlie@CBeirnaert-20150618-092033.uantwerpen.be>
2017-07-19 13:54:08 +0200

    added BatchSplitR function and first draft of RTcorrChecker

---
+++ R/BatchSplitR.R
@@ -0,0 +1,62 @@
+#' Function to split up xcms object by catch to perform a within batch analysis and merge the results at the end
+#'
+#' binnen batch analyse 
+#' gebruik obiwarp om te weten hoe je de features van batch 1 en 2 aan elkaar moet plakken
+#' doe dan analyse loess, PLSDA van batch 1 en 2 afzonderlijk. Giet alles in data frame (significant, fold change)
+# 
+#'
+#' @param xcmsObject XCMS object after multi-batch retention time correction and grouping.
+#' @param BatchClassLabel The name of the vector containing the Batch identifier (in xcmsObject@phenoData)
+#' @param BatchNames (optional) A vector with the names of the individual batches to consider eg c("Batch1", "Batch2"). Note that this is only if a subset of all the batches have to be selected. Otherwise supplying BatchClassLabel is sufficient. 
+#'
+#'   
+#' @return xcmsObject.split A list with in each list entry an xcms object of a single batch.
+#' 
+#'
+#' @author Charlie Beirnaert, \email{charlie.beirnaert@@uantwerpen.be}
+#'
+#' @examples
+#' 
+#' 
+#' @export
+BatchSplitR <- function(xcmsObject, BatchClassLabel = NULL, BatchNames = NULL ){
+    
+    if(!"xcmsSet" %in% class(xcmsObject)){
+        stop("xcmsObject is not of class 'xcmsSet'")
+    }
+    
+    if(is.null(BatchClassLabel)){
+        print(head(xcmsObject@phenoData))
+        BatchClassLabel <- readline(paste("Which of the following classes describes the batches: ",paste(colnames(xcmsObject@phenoData), collapse=" or ")))
+    }
+    
+    if(is.null(BatchNames)){
+        BatchNames = as.character(unique(xcmsObject@phenoData[BatchClassLabel])[,1])
+    } else if(!all(BatchNames %in% as.character(unique(xcmsObject@phenoData[BatchClassLabel])[,1]))) {
+        stop(paste("The supplied BatchNames could not all be matched to items in xcmsObject@phenoData which consists of:", paste(as.character(unique(xcmsObject@phenoData[BatchClassLabel])[,1]), collapse=" and ")))
+    }
+    
+    xcmsObject.split = list()
+    #mutual.RT.correction = list()
+    for(k in 1:length(BatchNames)){
+        split.samples = which(xcmsObject@phenoData[BatchClassLabel] == BatchNames[k])
+        
+        splitObject = xcmsObject
+        splitObject@peaks = splitObject@peaks[as.data.frame(xcmsObject@peaks)$sample %in% split.samples, ]
+        splitObject@groups = matrix(NA,ncol=0,nrow=0)
+        splitObject@groupidx = list()
+        splitObject@phenoData = xcmsObject@phenoData[split.samples,]
+        splitObject@rt$raw = xcmsObject@rt$raw[split.samples]
+        splitObject@rt$corrected = splitObject@rt$raw
+        splitObject@rt$IntraBatchRTCorrection = xcmsObject@rt$corrected[split.samples]
+        
+        xcmsObject.split[[k]] = splitObject
+        #mutual.RT.correction[[k]] = list(raw = xcmsObject@rt$raw[split.samples], corrected = xcmsObject@rt$corrected[split.samples])
+    }
+    
+    names(xcmsObject.split) = paste("xcms_",BatchNames,sep ="")
+    #names(mutual.RT.correction) = paste("xcms_",BatchNames,sep ="")
+    
+    return(xcmsObject.split)
+    
+}
---
+++ R/RTcorrChecker.R
@@ -0,0 +1,61 @@
+#' NOT FINISHED!!!!!! Function to compare different RT corrections by looking at the most shifted peaks from in method
+#'
+#' Filter out isotopes based on the PUTMEDID concept
+#'
+#' @param RTcorr1.xcmsGrouped XCMS object of first RT correction method (post RT correction and subsequent grouping)
+#' @param RTcorr2.xcmsGrouped XCMS object of second RT correction method (post RT correction and subsequent grouping)
+#'  
+#' 
+#'   
+#' @return 
+#' 
+#'
+#' @author Charlie Beirnaert, \email{charlie.beirnaert@@uantwerpen.be}
+#'
+#' @examples
+#' 
+#' 
+#' @export
+RTcorrChecker = function(RTcorr1.xcmsGrouped = test.rtcorrs.grouped[[1]] , 
+                         RTcorr2.xcmsGrouped = test.rtcorrs.obiwarp.grouped[[1]], 
+                         plotTitle.method1 = "Loess.correction, span = 1", 
+                         plotTitle.method2 = "Obiwarp correction, respons = 10",
+                         RTdiff.cutoff.value = NULL,
+                         RTdiff.cutoff.quantile = NULL){
+    
+    RT1.diffs = purrr::map2(.x = RTcorr1.xcmsGrouped@rt$corrected,
+                            .y = RTcorr1.xcmsGrouped@rt$raw,
+                            .f = ~ abs(.x - .y)
+    )
+    
+    RT2.diffs = purrr::map2(.x = RTcorr2.xcmsGrouped@rt$corrected,
+                            .y = RTcorr2.xcmsGrouped@rt$raw,
+                            .f = ~ abs(.x - .y)
+    )
+    
+    
+    
+    RTdiff.df = data.frame(RTdiffs = c(unlist(RT1.diffs), unlist(RT2.diffs)), 
+                           RTmethod = c(rep("method1", length(unlist(RT1.diffs))),
+                                        rep("method2", length(unlist(RT2.diffs)))) )
+    
+    ggplot(RTdiff.df, aes(RTdiffs,fill=RTmethod, colour = RTmethod))+ 
+        geom_histogram(alpha = 0.5, position = "identity") + 
+        theme_bw()
+    
+    
+    if(is.null(RTdiff.cutoff) & is.null(RTdiff.cutoff.quantile)){
+        warning("no RTdiff.cutoff or RTdiff.cutoff.quantile chosen, set to the 95 % quantile")
+        RTdiff.cutoff = quantile(RTdiff.df$RTdiffs, probs = 0.95)
+    } else if(is.null(RTdiff.cutoff)){
+        if(RTdiff.cutoff.quantile > 1 | RTdiff.cutoff.quantile < 0){
+            warning("RTdiff.cutoff.quantile not in [0,1] range. set to 0.95")
+            RTdiff.cutoff.quantile = 0.95
+        }
+        RTdiff.cutoff = quantile(RTdiff.df$RTdiffs, probs = RTdiff.cutoff.quantile)
+    }
+    
+    return(0)
+    
+    
+}
